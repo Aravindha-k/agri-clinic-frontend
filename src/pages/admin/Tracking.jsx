414b4a49cdbe45dd14dee641823822eaf999420a
Aravindha-k <siddhar91@gmail.com>
2026-02-01 17:27:29 +0530

    Fix tracking.css import case

--- src/pages/admin/Tracking.jsx
+++ src/pages/admin/Tracking.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../api/axios";
 
-import "./Tracking.css";
+import "./tracking.css";
+
 
 export default function Tracking() {
     const [employees, setEmployees] = useState([]);
